10089cf46f6fcd01223b3df0f6b3a0965c1d0e8f
shagamemnon <fwtaylor@syr.edu>
2016-02-10 22:32:55 -0500

    updated and restyled user profile to include new db attributes

--- vendor/tabs.js
+++ vendor/tabs.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function() {
-    $('.dashboard-container .sidebar a').on('click', function(e)  {
+    $('.profile-container .sidebar a').on('click', function(e)  {
         var currentAttrValue = jQuery(this).attr('href');
  
         // Show/Hide Tabs
-        $('.dashboard-container ' + currentAttrValue).show().siblings().hide();
+        $('.profile-container ' + currentAttrValue).show().siblings().hide();
  
         // Change/remove current tab to active
         $(this).parent('span').addClass('active').siblings().removeClass('active');
