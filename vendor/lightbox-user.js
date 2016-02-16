ec537027cb26e5bcbd588d395eecb536e4a51910
shagamemnon <fwtaylor@syr.edu>
2016-02-16 15:49:02 -0500

    resolve merge conflict

--- vendor/lightbox-user.js
+++ vendor/lightbox-user.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
   $("#open-user").click(function(){
      $('#overlay-user, #info-user').animate({'opacity':'0.7'}, 300, 'linear');
      $('#info-user').animate({'opacity':'1.00'},300,'linear');
-     $('#overlay-user, #info-user').css('display','block');
+     $('#overlay-user, #info-user').css('visibility','visible');
      $('#info-user').css({'left':(($(document).width()/2)-($('#info-user').width()/2))});
      $('#info-user').css({'top':(($(document).height()/2)-($('#info-user').height()/2)-50)});
   });
 
   $('#close-user').click(function(){
      $('#overlay-user, #info-user').animate({'opacity':'0'},300,'linear', function(){
-     $('#overlay-user, #info-user').css('display','none');
+     $('#overlay-user, #info-user').css('visibility','hidden');
    });
   });
 });
--- vendor/lightbox.js
+++ vendor/lightbox.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
   $("#open").click(function(){
      $('#overlay, #info').animate({'opacity':'0.7'}, 300, 'linear');
      $('#info').animate({'opacity':'1.00'},300,'linear');
-     $('#overlay, #info').css('display','block');
+     $('#overlay, #info').css('visibility','visible');
      $('#info').css({'left':(($(document).width()/2)-($('#info').width()/2))});
      $('#info').css({'top':(($(document).height()/2)-($('#info').height()/2)-50)});
   });
 
   $('#close').click(function(){
      $('#overlay, #info').animate({'opacity':'0'},300,'linear', function(){
-     $('#overlay, #info').css('display','none');
+     $('#overlay, #info').css('visibility','hidden');
    });
   });
 });
