43faa3643cd06bfca479a559dcb0925e699b4ddc
shagamemnon <fwtaylor@syr.edu>
2016-03-04 17:02:16 -0500

    updated node version

--- vendor/lightbox.js
+++ vendor/lightbox.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $("#open").click(function(){
      $('#overlay, #info').animate({'opacity':'0.7'}, 300, 'linear');
      $('#info').animate({'opacity':'1.00'},300,'linear');
-     $('#overlay, #info').css('display','block');
+     $('#overlay, #info').css({'display':'block', 'visibility':'visible'});
      $('#info').css({'left':(($(document).width()/2)-($('#info').width()/2))});
      $('#info').css({'top':(($(document).height()/2)-($('#info').height()/2)-50)});
   });
