3f54b804696c1db5073121cd5c43b2d6e766e326
georgiydubrov <georgiydubrov@gmail.com>
2016-03-21 18:15:17 +0200

    removed hardcoded messages from chat message template

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.parse = {
-    appId:  "xn6TXoZf5faH4PmG2zjdDqZfbkpYWojWoguouRZl", // "twqir8iySLnWPUlcjsm4fPCQnUtw4FKC5KO3gz6Z",//"ZowTKUYLhAaotnClMdlMrrPnG0xiQVHPWnHWuAf3",
+    appId: "xn6TXoZf5faH4PmG2zjdDqZfbkpYWojWoguouRZl", // "twqir8iySLnWPUlcjsm4fPCQnUtw4FKC5KO3gz6Z",//"ZowTKUYLhAaotnClMdlMrrPnG0xiQVHPWnHWuAf3",
     JSKey: "gPdPLvcYtNW3zNtoVZOizuZp29etRwOffAebK007", // "3aMFsko8xI84k1fdWMmiexsj8eNidaTemHYGPULe", //"n3U6xyK4bcJ1O9WPDh1rcADiY7C84UeRyebo096
     MsKey: "G29wx7YjlZ1q9gL7gfS0wzTMELlGbi6V6VISgb6k"// "ORon0LBiI5wxG0RgbYzGirpmPdOXXB6x6FQhNEHH"
 };
